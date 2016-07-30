f7d2c07ea386d08222c58709f92fe83b16bc1431
jal7027 <jal7027@gmail.com>
2016-07-29 20:50:57 -0700

    added video player framework

---
+++ Video Player/Video Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Video Player
+//
+//  Created by Joseph Leiber on 7/29/16.
+//  Copyright © 2016 Reduct Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
