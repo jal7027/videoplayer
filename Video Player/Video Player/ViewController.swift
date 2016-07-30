0da32c35da0a2b4ccb7f49afb1d56bfd2e87bcf0
jal7027 <jal7027@gmail.com>
2016-07-29 23:24:45 -0700

    added some FirebaseStorage. Fairly pretty, but not yet gorgeous.

--- Video Player/Video Player/ViewController.swift
+++ Video Player/Video Player/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import MobilePlayer
+import Firebase
+import FirebaseStorage
 
 class ViewController: UIViewController {
+    
+    let url: NSURL = NSURL(fileURLWithPath: "gs://hackathon-go.appspot.com/JaggedKeyGalapagosalbatross.mp4")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        FIRApp.configure()
+        
+//        let storage = FIRStorage.storage()
+//        let storageRef = storage.referenceForURL("gs://hackathon-go.appspot.com")
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let playerVC = MobilePlayerViewController(contentURL: url)
+        playerVC.title = "Vanilla Player - \(url)"
+        playerVC.activityItems = [url] // Check the documentation for more information.
+        presentMoviePlayerViewControllerAnimated(playerVC)
     }
 
     override func didReceiveMemoryWarning() {
