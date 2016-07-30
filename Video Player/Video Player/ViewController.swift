4840fac2f2506c7a5d482632c24976a979113077
jal7027 <josephaaronleiber@gmail.com>
2016-07-30 10:30:41 -0700

    firebase download works

--- Video Player/Video Player/ViewController.swift
+++ Video Player/Video Player/ViewController.swift
@@ -11,10 +11,23 @@ import MobilePlayer
 import Firebase
 import FirebaseStorage
 
+extension NSFileManager {
+    class func documentsDir() -> String {
+        var paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as [String]
+        return paths[0]
+    }
+    
+    class func cachesDir() -> String {
+        var paths = NSSearchPathForDirectoriesInDomains(.CachesDirectory, .UserDomainMask, true) as [String]
+        return paths[0]
+    }
+}
 
 class ViewController: UIViewController {
     
-    let url: NSURL = NSURL(fileURLWithPath: "gs://hackathon-go.appspot.com/JaggedKeyGalapagosalbatross.mp4")
+//    let url: NSURL = NSURL(fileURLWithPath: "gs://hackathon-go.appspot.com/JaggedKeyGalapagosalbatross.mp4")
+    
+    var storageRef : FIRStorageReference?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,22 +35,48 @@ class ViewController: UIViewController {
         
         FIRApp.configure()
         
-//        let storage = FIRStorage.storage()
-//        let storageRef = storage.referenceForURL("gs://hackathon-go.appspot.com")
+        let storage = FIRStorage.storage()
+        storageRef = storage.referenceForURL("gs://hackathon-go.appspot.com/JaggedKeyGalapagosalbatross.mp4")
+//        storageRef = storage.referenceForURL("gs://hackathon-go.appspot.com")
         
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        /*
         let playerVC = MobilePlayerViewController(contentURL: url)
         playerVC.title = "Vanilla Player - \(url)"
         playerVC.activityItems = [url] // Check the documentation for more information.
-        presentMoviePlayerViewControllerAnimated(playerVC)
+//        presentMoviePlayerViewControllerAnimated(playerVC)
+         */
+        download()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //
+    
+    func download() {
+        // Create a reference to the file you want to download
+//        let islandRef = storageRef.child("images/island.jpg")
+//        // Create local filesystem URL
+//        let localURL: NSURL! = NSURL(string: "file:///local/images/island.jpg")
+        
+        let doc = NSURL(fileURLWithPath:NSFileManager.cachesDir()).URLByAppendingPathComponent("testVideo.mp4")
+        
+        // Download to the local filesystem
+        storageRef?.writeToFile(doc) { (URL, error) in
+            if (error != nil) {
+                // Uh-oh, an error occurred!
+                print(error)
+            } else {
+                // Local file URL for "images/island.jpg" is returned
+                print(URL)
+            }
+        }
     }
 
 
